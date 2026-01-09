20ac3c363838a843a930a5f595a5fbc1ae20e62a
Tjark Pfeiffer <wir146@haw-hamburg.de>
2026-01-09 14:58:22 +0100

    erstes mal die sim zum laufen bekommen

---
+++ robot_node.java
@@ -0,0 +1,58 @@
+import org.cads.vs.roboticArm.hal.ICaDSRoboticArm;
+import org.cads.vs.roboticArm.hal.simulation.CaDSRoboticArmSimulation;
+import java.net.*;
+import java.io.*;
+
+//für terminal:
+//compile:
+//javac -cp "CaDSPracticalExamVS/VSEclipseExampleProject/resources/libs/*" robot_node.java
+//run: mit module-path für javafx
+//java -cp "CaDSPracticalExamVS/VSEclipseExampleProject/resources/libs/*" --module-path "C:/Users\pfeif/openjfx-21.0.2_windows-x64_bin-sdk/javafx-sdk-21.0.2/lib" --add-modules=javafx.controls,javafx.fxml robot_node.java
+public class robot_node {
+    private ICaDSRoboticArm roboticArm;
+
+    public robot_node() {
+        //real oder sim
+        //roboticArm = new CaDSRoboticArmReal(roboticArmHostAddress, roboticArmHostPort);
+        roboticArm = new CaDSRoboticArmSimulation();
+    }
+
+    public void moveArm(String move, int val) throws InterruptedException {
+        switch (move) {
+            case "leftRight":
+                roboticArm.setLeftRightPercentageTo(val);
+                break;
+            case "upDown":
+                roboticArm.setUpDownPercentageTo(val);
+                break;
+            case "backForth":
+                roboticArm.setBackForthPercentageTo(val);   
+                break;
+            case "openClose":
+                roboticArm.setOpenClosePercentageTo(val);
+                break;
+            default:
+                break;
+        }
+        Thread.sleep(1000); // wait for movement to complete
+    }
+
+    public static void main(String[] args) {
+        try {
+            robot_node robot = new robot_node();
+            System.out.println("Moving arm to 100% positions");
+            robot.moveArm("leftRight", 100);
+            robot.moveArm("upDown", 100);
+            robot.moveArm("backForth", 100);
+            robot.moveArm("openClose", 100);
+            Thread.sleep(6000);
+            System.out.println("Moving arm to 0% positions");
+            robot.moveArm("leftRight", 0);
+            robot.moveArm("upDown", 0);
+            robot.moveArm("backForth", 0);
+            robot.moveArm("openClose", 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
